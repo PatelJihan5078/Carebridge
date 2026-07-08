ceb3ff6d302f72b4c78a6cbaad7eda11ffada1cd
Patel Jihan <jihan7850785@gmail.com>
2026-07-08 21:59:33 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+let name = prompt("What is your name?")
+console.log("Hello, " + name + "!")
